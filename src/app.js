f1cd8194d4487140a91a62e9389c0726ec716b10
DEPIDSVY <depidsvy@nintendo.de>
2017-01-24 18:32:27 +0100

    - ADD service worker class - ADD notifications working

--- src/app.js
+++ src/app.js
@@ -5,3 +5,64 @@ const panels = require('./panels.tag');
 const card = require('./card.tag');
 
 riot.mount('*');
+
+global.shareMe = () => {
+	if (navigator.share) {
+		navigator.share({
+			title: document.title,
+			text: "Find interesting places nearby where-ever you are.",
+			url: window.location.href
+		}).then(() => console.log('Successful share'))
+		.catch(error => console.log('Error sharing:', error));
+	}
+	return false;
+};
+
+initializeServiceWorker(initialiseState);
+
+function initializeServiceWorker(callback) {
+	if ('serviceWorker' in navigator) {
+		console.log('service worker is supported');
+		window.addEventListener('load', function () {
+			console.log('registering sw');
+			navigator.serviceWorker.register('sw.js')
+			.then(function (registration) {
+				// Registration was successful
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			})
+			.then(() => {
+				console.log('calling callback...');
+				callback();
+			})
+			.catch(function (err) {
+				// registration failed :(
+				console.log('ServiceWorker registration failed: ', err);
+			});
+		});
+	} else {
+		console.log('service worker is NOT supported');	
+	}
+}
+
+function initialiseState() {
+	console.log('initializing state?');
+	if (Notification.permission == 'granted') {
+		console.log('We can send notifications already.');
+	} else if (Notification.permission === 'blocked') {
+		/* the user has previously denied push. Can't reprompt. */
+		console.log('User blocked notifications permanently.');
+	} else {
+		/* show a prompt to the user */
+		console.log('asking for permission...');
+		Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        console.log('notification are grated');
+      } else {
+				console.warn('notifications rejected');
+			}
+    });
+	}
+
+}
+// initialiseState();
---
+++ sw.ts
@@ -0,0 +1,117 @@
+/// <reference path="typings/index.d.ts" />
+// https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
+
+class ServiceWorker {
+	title = 'Have you been here?';
+	CACHE_NAME = 'my-site-cache-v1';
+	urlsToCache = [
+		// 'index.html',
+		// 'js/bundle.js',
+	];
+
+	constructor() {
+		self.addEventListener('install', function (event) {
+			// Perform install steps
+			console.log('service worker installed');
+			event.waitUntil(
+				caches.open(this.CACHE_NAME)
+					.then(function (cache) {
+						console.log('Opened cache');
+						//return cache.addAll(this.urlsToCache);
+					})
+			);
+		});
+
+		// auto cache everything
+		// disabled for development
+		self.addEventListener('no-fetch', this.cacheAllFetch.bind(this));
+
+
+		self.addEventListener('activate', (event) => {
+			console.log('activated');
+			//this.notify();
+			setInterval(this.periodicUpdater.bind(this), 10000);
+		});
+
+		self.addEventListener('push', event => {
+			this.notify();
+			event.waitUntil(() => {
+				// Process the event and display a notification.
+				//this.notify();
+			});
+		});
+
+		self.addEventListener('notificationclick', event => {
+			// Do something with the event
+			event.notification.close();
+			console.log('clicked');
+		});
+
+		self.addEventListener('notificationclose', event => {
+			// Do something with the event
+			console.log('closed');
+		});
+	}
+
+	notify() {
+		self.registration.showNotification(this.title, {
+			body: 'We found some interesting places nearby Want to see details?',
+			icon: 'img/map_blue.png',
+			vibrate: [200, 100, 200, 100, 200, 100, 400],
+			tag: 'request',
+			_actions: [
+				{action: 'yes', title: 'Yes!', icon: 'images/thumb-up.png'},
+				{action: 'no', title: 'No', icon: 'images/thumb-down.png'}
+			]
+		});
+	}
+
+	cacheAllFetch(event) {
+		event.respondWith(
+			caches.match(event.request)
+				.then(function (response) {
+					// Cache hit - return response
+					if (response) {
+						return response;
+					}
+
+					// IMPORTANT: Clone the request. A request is a stream and
+					// can only be consumed once. Since we are consuming this
+					// once by cache and once by the browser for fetch, we need
+					// to clone the response.
+					let fetchRequest = event.request.clone();
+
+					return fetch(fetchRequest).then(
+						function (response) {
+							// Check if we received a valid response
+							if (!response || response.status !== 200 || response.type !== 'basic') {
+								return response;
+							}
+
+							// IMPORTANT: Clone the response. A response is a stream
+							// and because we want the browser to consume the response
+							// as well as the cache consuming the response, we need
+							// to clone it so we have two streams.
+							let responseToCache = response.clone();
+
+							caches.open(this.CACHE_NAME)
+								.then(function (cache) {
+									if (response.method == 'GET') {
+										cache.put(event.request, responseToCache);
+									}
+								});
+
+							return response;
+						}
+					);
+				})
+		);
+	}
+
+	periodicUpdater() {
+		console.log('10000 seconds passed');
+	}
+
+}
+
+new ServiceWorker();
